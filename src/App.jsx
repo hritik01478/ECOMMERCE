4a85809671ae09ec034067db7dc9b15f9283930b
hritik01478 <hritikkumar01478@gmail.com>
2022-06-24 23:21:28 +0530

    cart page made

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Product />
+  return <Cart />
 };
 
 export default App;
